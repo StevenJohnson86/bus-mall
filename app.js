65ec17db1159d119ed5d0c2c675bca87e9d5c41a
Steven Johnson <stevenjohnson86@gmail.com>
2017-01-17 18:03:40 -0800

    reworked renderer, added three distinct buttons

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ var imgs = [['bag', 'img/bag.jpg'],
 ['wine-glass', 'img/wine-glass.jpg']];
 
 var dupChk = [];
+var imgIndex = [];
 // var shownCountSum = 0; NOT NECESSARY (yet) FOR SCOPE OF THIS PROJECT
 //
 // function shownSumr() {
@@ -71,19 +72,57 @@ function imgDupChk() {//checks img for duplicate in current set or previous set
     if (dupChk.includes(imgs[rand])) {
     } else {
       dupChk.push(imgs[rand]);
+      imgIndex.push(rand);
     }
   }
-  //call imgRender?
+  imgRender();
 }
 
-function imgRender() {
+// function nameMatch(imgName) {
+//   return imgName === dupChk[0].name;
+// }
+
+function imgRender() {//renders 3 images from dupChk Array to viewport
   var viewPort = document.getElementById('image-viewport');
-  for (var i = 0; i < 3; i++) {
-    var imgEl = document.createElement('img');
-    imgEl.setAttribute('src', dupChk[0].filePath);
-    viewPort.appendChild(imgEl);
-    dupChk.splice(0,1);
-  }
+
+  var leftImg = imgs[imgIndex[0]];
+  leftImg.shownCount += 1;
+  var lButEl = document.createElement('button');
+  lButEl.setAttribute('type', 'button');
+  lButEl.setAttribute('name', 'buttonLeft');
+  lButEl.setAttribute('onclick', 'submit');
+  var lImgEl = document.createElement('img');
+  lImgEl.setAttribute('src', leftImg.filePath);
+  lImgEl.setAttribute('id', leftImg.name);
+  lButEl.appendChild(lImgEl);
+  viewPort.appendChild(lButEl);
+
+  var centerImg = imgs[imgIndex[1]];
+  centerImg.shownCount += 1;
+  var cButEl = document.createElement('button');
+  cButEl.setAttribute('type', 'button');
+  cButEl.setAttribute('name', 'buttonCenter');
+  cButEl.setAttribute('onclick', 'submit');
+  var cImgEl = document.createElement('img');
+  cImgEl.setAttribute('src', centerImg.filePath);
+  cImgEl.setAttribute('id', centerImg.name);
+  cButEl.appendChild(cImgEl);
+  viewPort.appendChild(cButEl);
+
+  var rightImg = imgs[imgIndex[2]];
+  rightImg.shownCount += 1;
+  var rButEl = document.createElement('button');
+  rButEl.setAttribute('type', 'button');
+  rButEl.setAttribute('name', 'buttonRight');
+  rButEl.setAttribute('onclick', 'submit');
+  var rImgEl = document.createElement('img');
+  rImgEl.setAttribute('src', rightImg.filePath);
+  rImgEl.setAttribute('id', rightImg.name);
+  rButEl.appendChild(rImgEl);
+  viewPort.appendChild(rButEl);
+
+  dupChk.splice(0,3);
+  imgIndex.splice(0,3);
 }
 
 //-----------------calls----------------------------------------------
